9f7aaae900730a448757a48cd1c468d222e2531d
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-11-02 19:50:42 -0400

    refactor

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use Illuminate\Database\Eloquent\{Casts\Attribute, Factories\HasFactory, SoftDeletes};
+use Illuminate\Database\Eloquent\{Factories\HasFactory, SoftDeletes};
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
@@ -31,12 +31,5 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function fullName(): Attribute
-    {
-        return Attribute::make(
-            get: fn () => "{$this->first_name} {$this->last_name}",
-        );
     }
 }
