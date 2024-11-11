5ac7e9fea0264a63b4d4785615130e3d34515e84
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-11-10 22:26:19 -0400

    seed roles

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\RoleEnum;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Role::create(['name' => RoleEnum::Admin]);
+        Role::create(['name' => RoleEnum::User]);
+    }
+}
