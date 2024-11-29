93205e7ed275e9688aca83d5b95916a9d82570bc
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-11-28 21:03:02 -0400

    add Task model and migration

---
+++ app/Models/Task.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\{Model, SoftDeletes};
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    public function casts(): array
+    {
+        return [
+            'status' => TaskStatus::class,
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
