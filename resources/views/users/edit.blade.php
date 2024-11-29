7f7aac462aef92e25763482736a98975758acd01
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-11-28 21:06:13 -0400

    fix typo

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Create New User') }}
+            {{ __('Edit User') }} #{{ $user->id }}
         </h2>
     </x-slot>
 
