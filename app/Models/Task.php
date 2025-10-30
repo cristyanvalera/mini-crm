92ce540ae663a4aa8d62ec97701745603045846e
Cristyan Valera <cristyan.valera.81@gmail.com>
2025-10-29 22:45:44 -0400

    Agregar observador para la clase Task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use App\Enums\TaskStatus;
+use App\Observers\TaskObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\{Model, SoftDeletes};
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+#[ObservedBy(TaskObserver::class)]
 class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
